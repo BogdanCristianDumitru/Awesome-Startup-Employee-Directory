9885fff369d33eec4365b8abd17365fee6ac6077
Bogdan <bogdan_dumitru_85@yahoo.com>
2024-02-23 10:44:24 -0500

    update

--- js/app.js
+++ js/app.js
@@ -12,6 +12,7 @@ const employeeSearch = document.getElementById('employee_search');
 fetch(urlAPI)
   .then(res => res.json())
   .then(res => res.results)
+  //.then(data => console.log(data))
   .then(displayEmployees)
   .catch(err => console.log(err))
 
@@ -70,6 +71,55 @@ fetch(urlAPI)
 
   // Function display modal
   function displayModal(index) {
+    let employee = employees[index];
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+    let street = employee.location.street;
+    let postcode = employee.location.coordinates.postcode;
+    let dob = employee.dob.date;
+    let phone = employee.phone; 
+
+    let date = new Date(dob);
+
+    const modalHTML = `
+        <img class="avatar" src="${picture.large}">
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr/>
+            <p>${phone}</p>
+            <p class="address">${street.number} ${street.name} ${postcode}</p>
+            <p>Birthday:${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+        <div class="modal-nav">
+            <button class="prev-btn" onclick="showPrevEmployee(${index})">Previous</button>
+            <button class="next-btn" onclick="showNextEmployee(${index})">Next</button>
+        </div>
+    `;
+
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+}
+
+// Calculate the index of the previous employee function
+function showPrevEmployee(currentIndex) {
+    let prevIndex = (currentIndex - 1 + employees.length) % employees.length;
+    displayModal(prevIndex); // Display modal for the previous employee
+}
+
+// Calculate the index of the next employee function 
+function showNextEmployee(currentIndex) {
+    let nextIndex = (currentIndex + 1) % employees.length;
+    displayModal(nextIndex); // Display modal for the next employee
+}
+
+  
+
+  /*
+  function displayModal(index) {
     let { name, dob, phone, email, location: { city, street, postcode}, picture } = employees[index];
 
     let date = new Date(dob.date);
@@ -106,7 +156,7 @@ fetch(urlAPI)
         let nextIndex = (currentIndex + 1) % employees.length;
         displayModal(nextIndex); // Display modal for the next employee
     }
-
+*/
 
    // Modal eventListener
     gridContainer.addEventListener('click', e => {
@@ -120,8 +170,9 @@ fetch(urlAPI)
     modalClose.addEventListener('click', () => {
         overlay.classList.add("hidden");
     });
-    
+
     // Filtered employees eventListener 
+
   employeeSearch.addEventListener('keyup', e => {
     let currentValue = e.target.value.toLowerCase();
 
